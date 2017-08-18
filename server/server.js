7d3f8b84070dc0e7cce4ef8c92317c177a31ca02
Amaro <amarito93@gmail.com>
2017-08-18 07:02:36 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -15,8 +15,17 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc) => {
+    todo.save().then( doc => {
         res.status(201).send(doc);
+    }, (error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.get('/todos', (req, res) => {
+
+    Todo.find().then( todos => {
+        res.status(200).send({todos});
     }, (error) => {
         res.status(400).send(error);
     });
