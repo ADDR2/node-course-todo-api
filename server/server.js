477558859ad5be7ccea673fa05b1a1e162202f9b
Amaro <amarito93@gmail.com>
2017-08-19 17:15:55 -0400

    Not deploying to heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
