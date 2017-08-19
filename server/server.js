258451d77f43e733c584dc4c4743cdcc484f7da6
Amaro <amarito93@gmail.com>
2017-08-19 15:53:08 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { MongoClient, ObjectID } = require("mongodb");
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -29,6 +30,21 @@ app.get('/todos', (req, res) => {
     }, (error) => {
         res.status(400).send(error);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+
+    if(!ObjectID.isValid(id))
+        res.status(404).send("No valid id");
+    else{
+        Todo.findById(id).then( todo => {
+            if(!todo) res.status(404).send("No found id");
+            else res.status(200).send(todo);
+        }, (error) => {
+            res.status(400).send(error);
+        });
+    }
 });
 
 app.listen(PORT, () => {
