4a367ee7fc19d78c27d372de3f7972d6266b9990
Amaro <amarito93@gmail.com>
2017-08-23 21:56:30 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -39,6 +39,16 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(
+        response => {
+            res.status(200).send();
+        }, error => {
+            res.status(400).send();
+        }
+    );
+});
+
 app.post('/users/login', (req, res) => {
     const body = _.pick(req.body, ["email", "password"]);
 
